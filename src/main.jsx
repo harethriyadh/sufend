66b8e0f3cc9cca069a7cc1d6ffce58601eb6ecf5
harethriyadh <hareth.cn12012@gmail.com>
2025-03-27 17:54:11 +0300

    asdf

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import LoginPage from './auth/login';
 import RegisterPage from "./auth/register";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import IndexPage from "../public/index"; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,7 +13,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} /> Add this route 
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/index" element={<IndexPage />} /> 
       </Routes>
     </BrowserRouter>
   </StrictMode>
